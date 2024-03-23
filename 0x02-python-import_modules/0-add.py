ce2f1770c624f82be1aefdcc74f52b743255ef10
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-03-23 17:47:57 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-
-from add_0 import add as fake_add
-
 if __name__ == "__main__":
-    result = fake_add(a, -b)
+    """The sum of 1 and 2."""
+    from add_0 import add
+    
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, result))
