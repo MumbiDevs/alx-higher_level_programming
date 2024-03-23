521be4a7541fa661bef3034ba4501db8dced3773
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-03-23 17:44:55 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,8 @@
 a = 1
 b = 2
 
-from add_0 import add
+from add_0 import add as fake_add
 
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = fake_add(a, -b)
+    print("{} + {} = {}".format(a, b, result))
