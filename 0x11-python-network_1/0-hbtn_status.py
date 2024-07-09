a8fd0d3c45c92ef4463656f12ed859e40963de15
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-09 15:17:43 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,13 @@
 import urllib.request
+import urllib.parse
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+url = 'https://alx-intranet.hbtn.io/status'
+values = {'type' : '<class 'bytes'>',
+          'content' : 'OK',
+          'utf8 content' : 'OK'}
+data = urllib.parse.urlencode(values)
+data - data.encode('ascii')
+req = urllib.request.Request(url, data)
 with urllib.request.urlopen(req) as response:
   the_page - response.read()
 
