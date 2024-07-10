a0678c377ef202d425ff33d55c72f3215721299f
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-10 11:01:11 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const messages = [
-  "C is fun",
-  "Python is cool",
-  "JavaScript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (const message of messages) {
