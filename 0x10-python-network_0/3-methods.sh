48bc3415b1b951c8697b394cc6505e6d319518fe
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-23 00:47:15 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+URL=$1
+
+# Send an OPTIONS request and extract the Allow header
+HTTP_METHODS=$(curl -sI -X OPTIONS "$URL" | grep 'Allow:' | cut -d ' ' -f 2-)
+
+if [ -z "$HTTP_METHODS" ]; then
+    echo "No Allow header found or server does not accept any methods."
+else
+    echo $HTTP_METHODS
+fi
