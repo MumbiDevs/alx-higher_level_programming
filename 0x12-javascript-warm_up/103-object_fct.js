3b75e5ec344b9899935c2fe584c84ac41f7c2110
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-23 00:07:57 +0300

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,17 +2,15 @@
 
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
+};
+
+// Define incr function that increments the value property
+myObject.incr = function () {
+  this.value++;
 };
 
 console.log(myObject);
-
-myObject.incr = function () { // Redefine incr as an anonymous function
-  this.value++;
-};
 
 myObject.incr();
 console.log(myObject);
